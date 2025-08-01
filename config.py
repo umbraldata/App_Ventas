c4fb7f68508499490840c033195fa229ea7da8bf
TurcoN <umbraldatagithub@gmail.com>
2025-07-31 21:35:45 -0400

    base de dato migrada

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
+from dotenv import load_dotenv
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()  # Carga las variables del .env
 
 class Config:
     SECRET_KEY = 'clave-secreta-segura'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False

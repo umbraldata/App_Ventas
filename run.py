a72bf209044c53272fa90675c04d467721b2ebe8
TurcoN <umbraldatagithub@gmail.com>
2025-08-01 10:46:07 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+from app import app
 import os
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
